de17cc3682387cffde34c84c3dce5a84ab1e9875
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2018-05-30 13:21:03 -0300

    Menu com efeitos e parciais adicionadas

--- src/main.js
+++ src/main.js
@@ -1,19 +1,27 @@
 window.onload = () => {
     const welcome = document.getElementById('welcome');
+    
     write(welcome, 'Bem vindo!');
-    setTimeout(() => {
-        welcome.innerHTML = '';
-        write(welcome, 'Somos a RM Tecnologia!');
-    }, 3000);
+    setTimeout(() => write(welcome, 'Somos a RM Tecnologia!'), 3000);
+    setTimeout(() => showHeader(), 5000);
+}
 
+const showHeader = () => {
+    const links = Array.from(document.getElementsByTagName('h4'));
+    links.forEach(e => e.style.opacity = 1);
+    
     const lnkAbout = document.getElementById('lnk-about')
-    lnkAbout.addEventListener('click', _ => {
-        document.getElementById('about').style.display = 'initial';
-        document.getElementById('welcome').style.display = 'none';
-    });
+    lnkAbout.addEventListener('click', _ => loadHTML('about'));
+    
+    const lnkContact = document.getElementById('lnk-contact')
+    lnkContact.addEventListener('click', _ => loadHTML('contact'));
+    
+    const lnkProjects = document.getElementById('lnk-projects')
+    lnkProjects.addEventListener('click', _ => loadHTML('projects'));
 }
 
 const write = (el, message) => {
+    el.innerHTML = '';
     let i = 0;
     const speed = 100;
     const typeWriter = () => {
@@ -24,6 +32,17 @@ const write = (el, message) => {
         }
     }
     typeWriter();
+}
+
+const loadHTML = (page) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `partial/${page}.html`, true);
+    xhr.onreadystatechange = function () {
+        if (this.readyState !== 4) return;
+        if (this.status !== 200) return;
+        document.getElementById('welcome').innerHTML = this.responseText;
+    };
+    xhr.send();
 }
 
 if ('serviceWorker' in navigator) {
